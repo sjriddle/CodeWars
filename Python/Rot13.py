0e64a490ab9da800ca28437dc9058464dd9f64cd
Samuel Riddle <30331129+sjriddle@users.noreply.github.com>
2018-02-14 15:36:30 -0700

    Update Rot13.py

--- Python/Rot13.py
+++ Python/Rot13.py
@@ -1,9 +1,7 @@
-import string  # forced import by CodeWars kata
-from string import ascii_uppercase as up, ascii_lowercase as low, \
-    ascii_letters as az, maketrans
+import string
+from string import ascii_uppercase as up, ascii_lowercase as low, ascii_letters as az, maketrans
 
 ROT13 = maketrans(low[13:] + low[:13] + up[13:] + up[:13], az)
-
 
 def rot13(message):
     return message.translate(ROT13)
