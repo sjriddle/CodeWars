feaa3472c2bfaab967a974837f4a6788db0ddedb
Samuel Riddle <30331129+sjriddle@users.noreply.github.com>
2018-05-22 08:47:59 -0600

    Update FindDivisor.js

--- Javascript/FindDivisor.js
+++ Javascript/FindDivisor.js
@@ -1,7 +1,9 @@
 function divisors(int) {
     let out = [];
-    for (let i=2; i<int; i++) {
-      if (int%i === 0) out.push(i);
+    for(let i=2; i<int; i++) {
+      if(int%i === 0) {
+          out.push(i);
+      }
     }
     return out.length === 0 ? `${int} is prime` : out;
   }
